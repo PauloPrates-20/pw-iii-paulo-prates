e431792a07193ffc91bb6b0b616ee4500d5021e0
Paulo Prates <paulo.prates01@hotmail.com>
2024-11-06 14:39:43 -0300

    refactor: renamed get-items return to data

--- listadecompras/app/api/get-items/route.ts
+++ listadecompras/app/api/get-items/route.ts
@@ -3,28 +3,28 @@ import { getDocs, collection } from 'firebase/firestore';
 import { Item, Res } from '@/app/lib/definitions';
 
 export async function GET() {
-    const items: Item[] = [];
+    const data: Item[] = [];
     const res: Res = { status: true, errors: [] };
 
     try {
         const querySnapshot = await getDocs(collection(db, 'items'));
 
         querySnapshot.forEach(doc => {
-            const data = doc.data();
+            const docData = doc.data();
 
             const item: Item = {
                 id: doc.id,
-                name: data.name,
-                quantity: data.quantity,
-                checked: data.checked,
+                name: docData.name,
+                quantity: docData.quantity,
+                checked: docData.checked,
             };
 
-            items.push(item);
+            data.push(item);
         });
     } catch (error) {
         res.status = false;
         res.errors.push('Erro ao ler itens da lista');
     }
 
-    return Response.json({ res, items });
+    return Response.json({ res, data });
 }
