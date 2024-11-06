7adb25c8271c142768cda75839cb9eb55d1ad1bc
Paulo Prates <paulo.prates01@hotmail.com>
2024-11-06 14:43:28 -0300

    fix: removed extra id prop from update-item

--- listadecompras/app/api/update-item/route.ts
+++ listadecompras/app/api/update-item/route.ts
@@ -1,13 +1,19 @@
 import { db } from '@/app/lib/firestore-config';
 import { updateDoc, doc } from 'firebase/firestore';
-import { Item, Res } from '@/app/lib/definitions';
+import { Item, ItemData, Res } from '@/app/lib/definitions';
 
 export async function POST(request: Request) {
     const data = await request.json() as Item;
     const res: Res = { status: true, errors: [] };
 
     try {
-        await updateDoc(doc(db, 'items', data.id), { ...data });
+        const docData: ItemData = {
+            name: data.name,
+            quantity: data.quantity,
+            checked: data.checked,
+        };
+
+        await updateDoc(doc(db, 'items', data.id), { ...docData });
     } catch (error) {
         res.status = false;
         res.errors.push('Erro ao editar item');
