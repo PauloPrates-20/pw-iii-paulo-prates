30eaef4bd67f8c601d32c3cb9cd1aa904cdb7fd7
Paulo Prates <paulo.prates01@hotmail.com>
2024-11-07 12:53:51 -0300

    style: padding for the main content

--- listadecompras/app/layout.tsx
+++ listadecompras/app/layout.tsx
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className='p-4'>
+          {children}
+        </main>
       </body>
     </html>
   );
